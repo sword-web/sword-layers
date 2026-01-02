a5a0c7fa3d7c300b006fb582ecb297309cb44698
MrRevillod <lrevillod2022@alu.uct.cl>
2026-01-02 00:20:15 -0300

    fix: move extension mapper to isolated `tower::Service`

--- src/socketio/layer.rs
+++ src/socketio/layer.rs
@@ -7,41 +7,34 @@ use tower_layer::{Identity, Stack};
 
 pub struct SocketIoServerLayer;
 
-pub type SocketIoRequestFnMapperServiceLayer<T> =
-    ServiceBuilder<Stack<MapRequestLayer<T>, Identity>>;
+// pub type SocketIoRequestFnMapperServiceLayer<T> =
 
 impl SocketIoServerLayer {
-    pub fn new(
-        config: SocketIoServerConfig,
-    ) -> (
-        SocketIoLayer,
-        SocketIo,
-        SocketIoRequestFnMapperServiceLayer<impl FnMut(Request) -> Request>,
-    ) {
+    pub fn new(config: &SocketIoServerConfig) -> (SocketIoLayer, SocketIo) {
         let mut layer_builder = SocketIo::builder();
 
-        if let Some(ack_timeout) = config.ack_timeout {
+        if let Some(ack_timeout) = &config.ack_timeout {
             layer_builder = layer_builder.ack_timeout(ack_timeout.parsed);
         }
-        if let Some(connect_timeout) = config.connect_timeout {
+        if let Some(connect_timeout) = &config.connect_timeout {
             layer_builder = layer_builder.connect_timeout(connect_timeout.parsed);
         }
-        if let Some(max_buffer_size) = config.max_buffer_size {
-            layer_builder = layer_builder.max_buffer_size(max_buffer_size);
+        if let Some(max_buffer_size) = &config.max_buffer_size {
+            layer_builder = layer_builder.max_buffer_size(*max_buffer_size);
         }
-        if let Some(max_payload) = config.max_payload {
+        if let Some(max_payload) = &config.max_payload {
             layer_builder = layer_builder.max_payload(max_payload.parsed as u64);
         }
-        if let Some(ping_interval) = config.ping_interval {
+        if let Some(ping_interval) = &config.ping_interval {
             layer_builder = layer_builder.ping_interval(ping_interval.parsed);
         }
-        if let Some(ping_timeout) = config.ping_timeout {
+        if let Some(ping_timeout) = &config.ping_timeout {
             layer_builder = layer_builder.ping_timeout(ping_timeout.parsed);
         }
-        if let Some(req_path) = config.req_path {
-            layer_builder = layer_builder.req_path(req_path);
+        if let Some(req_path) = &config.req_path {
+            layer_builder = layer_builder.req_path(req_path.clone());
         }
-        if let Some(transports) = config.transports {
+        if let Some(transports) = &config.transports {
             let parsed_transports = transports
                 .into_iter()
                 .collect::<HashSet<_>>()
@@ -78,12 +71,16 @@ impl SocketIoServerLayer {
 
         let (layer, io) = layer_builder.build_layer();
 
-        let extension_service = ServiceBuilder::new().map_request(move |mut req: Request| {
+        (layer, io)
+    }
+
+    pub fn extensions_service_mapper(
+        config: &SocketIoServerConfig,
+    ) -> ServiceBuilder<Stack<MapRequestLayer<impl FnMut(Request) -> Request>, Identity>> {
+        ServiceBuilder::new().map_request(move |mut req: Request| {
             req.extensions_mut()
                 .insert::<SocketIoParser>(config.parser.clone().unwrap_or_default());
             req
-        });
-
-        (layer, io, extension_service)
+        })
     }
 }
